c7a638d91f67cc15f15346f28e41ebf5f4bd6b7a
MatheusMMarques <88527569+MatheusMMarques@users.noreply.github.com>
2021-08-27 14:22:38 -0300

    Add files via upload

---
+++ energiaSolar.js
@@ -0,0 +1,3 @@
+document.getElementById("linkEnergiaSolar").addEventListener("click", function() {
+    window.open("https://www.portalsolar.com.br/energia-solar-como-funciona-entenda-em-5-minutos", "_blank")
+});
